7c8b7e5d8588d07cfcc888fca66575f297ee89c0
strrules105 <camobot3@gmail.com>
2021-09-05 11:59:42 -0400

    Adding first files

---
+++ TrustFlip/WebFrom1.aspx
@@ -0,0 +1,18 @@
+<%@PageLanguage="C#"AutoEventWireup="true"CodeBehind="WebForm1.aspx.cs"  
+  
+Inherits="TypeScriptWithWeb.WebForm1"%>  
+<!DOCTYPEhtml>  
+<html  
+    xmlns="http://www.w3.org/1999/xhtml">  
+    <headrunat="server">  
+        <title></title>  
+    </head>  
+    <body>  
+        <formid="form1"runat="server">  
+            <div>  
+                <scriptsrc="app.ts">  
+                </script>//ref of ts file  
+            </div>  
+        </form>  
+    </body>  
+</html>   
---
+++ TrustFlip/app.ts
@@ -0,0 +1,18 @@
+interface MyInterface {  
+    firstname: string;  
+    lastname: string;  
+    country: string;  
+   }  
+   class MyClass {  
+    fulldata: string;  
+    constructor(public firstname, public lastname, public country) {  
+     this.fulldata = firstname + " " + lastname + " " + country;  
+    }  
+   }  
+   function data(mcn: MyInterface) {  
+    return "Welcome in " + mcn.firstname + " " + mcn.lastname + " " + mcn.country;  
+   }  
+   window.onload = () => {  
+    var name = new MyClass("Mcn", "solution", " at India");  
+    document.body.innerHTML = data(name);  
+   }  
